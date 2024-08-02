fb384892fd5a0406c74ff4d0d63a4cf57e4e56ed
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-08-02 17:43:20 +0300

    [FEAT-11] Add creating experience lists "Add experience" button that allows to create multiple education input cv blocks

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,68 +1,102 @@
 /* eslint-disable react/prop-types */
 import "./experience.css";
 
-function Experience({ data, handleExperience }) {
+function Experience({
+  data,
+  handleExperience,
+  deleteExperience,
+  addExperience,
+}) {
   return (
     <section className="experience">
       <h2 className="blockTitle">Experience</h2>
-      <div className="content">
-        <label htmlFor="company">
-          <h3>Company Name</h3>
-          <input
-            type="text"
-            name="company"
-            id="company"
-            value={data.experience[0].text}
-            onChange={handleExperience}
-          />
-        </label>
 
-        <label htmlFor="position">
-          <h3>Position Title</h3>
-          <input
-            type="text"
-            name="position"
-            id="position"
-            value={data.experience[1].text}
-            onChange={handleExperience}
-          />
-        </label>
+      {data.experience.map((exp, index) => (
+        <div className="content" key={exp.id}>
+          {data.experience.length > 1 && (
+            <div className="experienceTitle">
+              <h3>{`Experience ${index + 1}`}</h3>
+              <button
+                aria-label="delete experience"
+                onClick={() => {
+                  deleteExperience(exp.id);
+                }}
+              >
+                X
+              </button>
+            </div>
+          )}
 
-        <div className="jobDuration">
-          <label htmlFor="jobStart">
-            <h3>Start Date</h3>
+          <label htmlFor={`company${index}`}>
+            <h3>Company Name</h3>
             <input
               type="text"
-              name="jobStart"
-              id="jobStart"
-              value={data.experience[2].text}
-              onChange={handleExperience}
+              name="company"
+              id={`company${index}`}
+              placeholder="Example: Ford"
+              value={exp.company}
+              onChange={(event) => handleExperience(event, exp.id)}
             />
           </label>
 
-          <label htmlFor="jobEnd">
-            <h3>End Date</h3>
+          <label htmlFor={`position${index}`}>
+            <h3>Position Title</h3>
             <input
               type="text"
-              name="jobEnd"
-              id="jobEnd"
-              value={data.experience[3].text}
-              onChange={handleExperience}
+              name="position"
+              id={`position${index}`}
+              placeholder="Example: Assistant Manager"
+              value={exp.position}
+              onChange={(event) => handleExperience(event, exp.id)}
+            />
+          </label>
+
+          <div className="jobDuration">
+            <label htmlFor={`jobStart${index}`}>
+              <h3>Start Date</h3>
+              <input
+                type="text"
+                name="jobStart"
+                id={`jobStart${index}`}
+                placeholder="Example: 2023"
+                value={exp.jobStart}
+                onChange={(event) => handleExperience(event, exp.id)}
+              />
+            </label>
+
+            <label htmlFor={`jobEnd${index}`}>
+              <h3>End Date</h3>
+              <input
+                type="text"
+                name="jobEnd"
+                id={`jobEnd${index}`}
+                placeholder="Example: 2024"
+                value={exp.jobEnd}
+                onChange={(event) => handleExperience(event, exp.id)}
+              />
+            </label>
+          </div>
+
+          <label htmlFor={`details${index}`}>
+            <h3>Details</h3>
+            <textarea
+              type="text"
+              name="details"
+              id={`details${index}`}
+              placeholder="Example: Worked on electronic systems of Ford vehicles"
+              value={exp.details}
+              onChange={(event) => handleExperience(event, exp.id)}
             />
           </label>
         </div>
-
-        <label htmlFor="details">
-          <h3>Details</h3>
-          <textarea
-            type="text"
-            name="details"
-            id="details"
-            value={data.experience[4].text}
-            onChange={handleExperience}
-          />
-        </label>
-      </div>
+      ))}
+      <button
+        aria-label="add experience"
+        className="add-experience"
+        onClick={addExperience}
+      >
+        + Experience
+      </button>
     </section>
   );
 }
