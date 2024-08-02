0582858fb6272bc3b0d2db18ffe66a2545dffdfe
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-08-02 17:35:14 +0300

    [FEAT-7] Add image input and split fullName into 2 fullName is splitted into First and Last name

--- src/components/generalinformation.jsx
+++ src/components/generalinformation.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/prop-types */
 import "./generalinformation.css";
 
-function GeneralInformation({ data, handlePerson }) {
+function GeneralInformation({ data, handlePerson, handleImg }) {
   return (
     <section className="general">
       <h2 className="blockTitle">General Information </h2>
 
       <div className="content">
-        <label htmlFor="fullName">
-          <h3>Full Name</h3>
+        <label htmlFor="firstname">
+          <h3>First Name</h3>
           <input
             type="text"
-            name="fullName"
-            id="fullName"
+            name="firstname"
+            placeholder="Example: John"
+            id="firstname"
             value={data.person[0].text}
+            onChange={handlePerson}
+          />
+        </label>
+        <label htmlFor="surname">
+          <h3>Surname</h3>
+          <input
+            type="text"
+            name="surname"
+            placeholder="Example: Doe"
+            id="surname"
+            value={data.person[1].text}
             onChange={handlePerson}
           />
         </label>
@@ -22,8 +34,9 @@ function GeneralInformation({ data, handlePerson }) {
           <input
             type="email"
             name="email"
+            placeholder="Example: example@email.com"
             id="email"
-            value={data.person[1].text}
+            value={data.person[2].text}
             onChange={handlePerson}
           />
         </label>
@@ -34,7 +47,8 @@ function GeneralInformation({ data, handlePerson }) {
             type="tel"
             name="number"
             id="telephone"
-            value={data.person[2].text}
+            placeholder="Example: +1 (XXX) XXX-XXXX"
+            value={data.person[3].text}
             onChange={handlePerson}
           />
         </label>
@@ -45,9 +59,14 @@ function GeneralInformation({ data, handlePerson }) {
             type="text"
             name="address"
             id="address"
-            value={data.person[3].text}
+            placeholder="Example: Istanbul, Turkey"
+            value={data.person[4].text}
             onChange={handlePerson}
           />
+        </label>
+        <label htmlFor="file">
+          <h3>Image</h3>
+          <input type="file" id="file" name="image" onChange={handleImg} />
         </label>
       </div>
     </section>
