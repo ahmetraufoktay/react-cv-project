4b1a049b1a7e12be44e3a0da642c2c4afb0b7c95
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-08-02 17:39:03 +0300

    [FEAT-9] Add creating education lists "Add education" button that allows to create multiple education input cv blocks

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,69 +1,98 @@
 /* eslint-disable react/prop-types */
 import "./education.css";
 
-function Education({ data, handleEducation }) {
+function Education({ data, handleEducation, deleteEducation, addEducation }) {
   return (
     <section className="education">
       <h2 className="blockTitle">Education</h2>
 
-      <div className="content">
-        <label htmlFor="school">
-          <h3>School</h3>
-          <input
-            type="text"
-            name="school"
-            id="school"
-            value={data.education[0].text}
-            onChange={handleEducation}
-          />
-        </label>
+      {data.education.map((edu, index) => {
+        return (
+          <div className="content" key={edu.id}>
+            {data.education.length > 1 && (
+              <div className="educationTitle">
+                <h3>{`Education ${index + 1}`}</h3>
+                <button
+                  aria-label="delete education"
+                  onClick={() => {
+                    deleteEducation(edu.id);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+            )}
+            <label htmlFor={`school${index}`}>
+              <h3>School</h3>
+              <input
+                type="text"
+                name="school"
+                placeholder="Example: Yildiz Technical University"
+                id={`school${index}`}
+                value={edu.school}
+                onChange={(event) => handleEducation(event, edu.id)}
+              />
+            </label>
 
-        <label htmlFor="degree">
-          <h3>Degree</h3>
-          <input
-            type="text"
-            name="degree"
-            id="degree"
-            value={data.education[1].text}
-            onChange={handleEducation}
-          />
-        </label>
+            <label htmlFor={`degree${index}`}>
+              <h3>Degree</h3>
+              <input
+                type="text"
+                name="degree"
+                placeholder="Example: Computer Engineering "
+                id={`degree${index}`}
+                value={edu.degree}
+                onChange={(event) => handleEducation(event, edu.id)}
+              />
+            </label>
 
-        <div className="degreeDuration">
-          <label htmlFor="degreeStart">
-            <h3>Start Date</h3>
-            <input
-              type="text"
-              name="degreeStart"
-              id="degreeStart"
-              value={data.education[2].text}
-              onChange={handleEducation}
-            />
-          </label>
+            <div className="degreeDuration">
+              <label htmlFor={`degreeStart${index}`}>
+                <h3>Start Date</h3>
+                <input
+                  type="text"
+                  name="degreeStart"
+                  placeholder="Example: 2023"
+                  id={`degreeStart${index}`}
+                  value={edu.degreeStart}
+                  onChange={(event) => handleEducation(event, edu.id)}
+                />
+              </label>
 
-          <label htmlFor="degreeEnd">
-            <h3>End Date</h3>
-            <input
-              type="text"
-              name="degreeEnd"
-              id="degreeEnd"
-              value={data.education[3].text}
-              onChange={handleEducation}
-            />
-          </label>
-        </div>
+              <label htmlFor={`degreeEnd${index}`}>
+                <h3>End Date</h3>
+                <input
+                  type="text"
+                  name="degreeEnd"
+                  placeholder="Example: 2028"
+                  id={`degreeEnd${index}`}
+                  value={edu.degreeEnd}
+                  onChange={(event) => handleEducation(event, edu.id)}
+                />
+              </label>
+            </div>
 
-        <label htmlFor="location">
-          <h3>Location</h3>
-          <input
-            type="text"
-            name="location"
-            id="location"
-            value={data.education[4].text}
-            onChange={handleEducation}
-          />
-        </label>
-      </div>
+            <label htmlFor={`gpa${index}`}>
+              <h3>GPA</h3>
+              <input
+                type="text"
+                name="gpa"
+                placeholder="Example: 3.5"
+                id={`gpa${index}`}
+                value={edu.gpa}
+                onChange={(event) => handleEducation(event, edu.id)}
+              />
+            </label>
+          </div>
+        );
+      })}
+      <button
+        aria-label="add education"
+        className="add-education"
+        onClick={addEducation}
+      >
+        + Education
+      </button>
     </section>
   );
 }
